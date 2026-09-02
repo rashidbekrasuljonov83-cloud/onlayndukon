85c3fccd6971d7de2932a5f68ca88b5ce43932bf
Rashidbek <rasishidbekrasuljonov83>
2026-09-02 15:47:25 +0300

    amaliyot

--- src/app/page.js
+++ src/app/page.js
@@ -3,17 +3,4 @@ import ProductCard from "@/components/ProductCard";
 import Navbar from "@/components/Navbar";
 import React from "react";
 
-export default function home() {
-  let res = await fetch("https://dummyjson.com/products");
-const products = await res.json();
-  return (
-    <div>
-      <Navbar  />
-       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-        {products.products.map((prod) => {
-          return <ProductCard key={prod.id} product={prod} />;
-        })}
-        </div>
-    </div>
-  );
-}
+export default function home() {}
